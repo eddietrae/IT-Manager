36a342f83b99e18fd2f3acaece7032106a7c3e3f
hillary.j.murphy@gmail.com <hillary.j.murphy@gmail.com>
2020-07-12 13:56:58 -0500

    I created the Employee, Ticket, and Store classes. I am wondering if we should also have classes for Severity and Status to limit the options that are selected for those fields. Please take a look and let me know your thoughts on this before we do any merging. I didn't really incorporate much from chapter 12 on how I set this up.

---
+++ Models/Ticket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace itmanager.Models
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a short description.")]
+        [StringLength(50)]
+        public string ShortDescription { get; set; }
+
+        [Required(ErrorMessage = "Please enter a detailed description.")]
+        public string DetailedDescription { get; set; }
+
+        // Wondering if this should be another class to restrict the options for severity?
+        [Required(ErrorMessage = "Please enter the severity")]
+        public string Severity { get; set; }
+
+        // Wondering if this should be another class to restrict the optiosn for status?
+        [Required(ErrorMessage = "Please enter the status")]
+        public string Status { get; set; }
+
+        [Required(ErrorMessage = "Please enter the store")]
+        public int StoreId { get; set; }
+        public Store Store { get; set; }
+
+        [Required(ErrorMessage = "Please enter the employee")]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
