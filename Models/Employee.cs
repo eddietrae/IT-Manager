dd545ee6138ad8588938d4610435e6f541c94f9a
hillary.j.murphy@gmail.com <hillary.j.murphy@gmail.com>
2020-07-13 15:36:32 -0500

    Adding classes in with Traes updates for views

---
+++ Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace itmanager.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Authority { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        // Foreign Key to Store Table
+        [Required]
+        public int StoreId { get; set; }
+        public Store Store { get; set; }
+    }
+}
---
+++ Models/Store.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace itmanager.Models
+{
+    public class Store
+    {
+        public int StoreId { get; set; }
+
+        public string StoreAlias { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Zip { get; set; }
+    }
+}
