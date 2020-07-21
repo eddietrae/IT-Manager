82b46cbdfe6cb85b6336e83cf684fd240357f2a5
hillary.j.murphy@gmail.com <hillary.j.murphy@gmail.com>
2020-07-20 21:17:36 -0500

    Updates to add TicketSession, SessionExtensions, and Filters. I still plan to look at Index view but haven't gotten there yet.

---
+++ Models/Filters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace itmanager.Models
+{
+    public class Filters
+    {
+        public Filters(string filterstring)
+        {
+            FilterString = filterstring ?? "all-all";
+            string[] filters = FilterString.Split('-');
+            SeverityId = filters[0];
+            StatusId = filters[1];
+        }
+        public string FilterString { get; }
+        public string SeverityId { get; }
+        public string StatusId { get; }
+
+        public bool HasSeverity => SeverityId.ToLower() != "all";
+        public bool HasStatus => StatusId.ToLower() != "all";
+
+    }
+}
---
+++ Models/TicketSession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace itmanager.Models
+{
+    public class TicketSession
+    {
+        private const string EmpKey = "employee";
+
+        private ISession session { get; set; }
+        public TicketSession(ISession session)
+        {
+            this.session = session;
+        }
+
+        public void SetActiveEmployee(string activeEmployee) =>
+            session.SetString(EmpKey, activeEmployee);
+        public string GetActiveEmployee() => session.GetString(EmpKey);
+
+        public void RemoveMyEmployees()
+        {
+            session.Remove(EmpKey);
+        }
+    }
+}
